ec8450d2f9684295c87fb4e42cb6e3414b2a48bf
David Wilemski <david@davidwilemski.com>
2014-04-23 22:46:20 -0400

    add some godoc comments

--- unit/distance.go
+++ unit/distance.go
@@ -1,12 +1,18 @@
-/*
-
-*/
-
 package unit
 
+/*
+Kilometers distance unit
+*/
 type Kilometers float64
+
+/*
+Miles distance unit
+*/
 type Miles float64
 
+/*
+A Distance interface implements conversion between various distance units
+*/
 type Distance interface {
 	Miles() float64
 	Kilometers() float64
@@ -15,26 +21,44 @@ type Distance interface {
 	ToKilometers() Kilometers
 }
 
+/*
+Miles returns a distance value (in miles)
+*/
 func (m Miles) Miles() float64 {
 	return float64(m)
 }
 
+/*
+Kilometers returns a distance value (in kilometers)
+*/
 func (m Miles) Kilometers() float64 {
 	return float64(m * 0.6)
 }
 
+/*
+ToMiles converts the distance value to a Miles type
+*/
 func (m Miles) ToMiles() Miles {
 	return m
 }
 
+/*
+ToKilometers converts the distance value to a Kilometerss type
+*/
 func (m Miles) ToKilometers() Kilometers {
 	return Kilometers(m.Kilometers())
 }
 
+/*
+Miles returns a distance value (in miles)
+*/
 func (k Kilometers) Miles() float64 {
 	return float64(k / 0.6)
 }
 
+/*
+Kilometers returns a distance value (in kilometers)
+*/
 func (k Kilometers) Kilometers() float64 {
 	return float64(k)
 }
